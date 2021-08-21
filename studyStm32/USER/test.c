6c88dfa88a383a314cdf14584903ea58f36be548
longmaoaiali <13213102491@163.com>
2021-08-21 11:15:28 +0800

    add extern interrupt

--- studyStm32/USER/test.c
+++ studyStm32/USER/test.c
@@ -2,6 +2,8 @@
 #include "sys.h"
 #include "led.h"
 #include "usart.h"
+#include "exti.h"
+#include "beep.h"
 
 int main()
 {
@@ -9,25 +11,12 @@ int main()
 	delay_init(72);
 	LED_Init();
 	uart_init(72,115200);
-	int len,i,times=0;
+	EXTI_Init();
+	BEEP_Init();
 	
 	while(1){
-		if(USART_RX_STA & 0x8000){
-				len = USART_RX_STA & 0x3FFF;
-				printf("you send message is \r\n\r\n");
-				for(i=0;i<len;i++){
-					USART1->DR = USART_RX_BUF[i];
-					while((USART1->SR&0x40)==0);//wait until send over
-				}
-				printf("\r\n");
-				USART_RX_STA=0;
-		} else {
-			times++;
-			if((times%30)==0){
-				LED0 = !LED0;
-			}
-			delay_ms(30);
-		}
+		printf("OK Liuyu\r\n");
+		delay_ms(1000);
 	}
 	
 }
--- studyStm32/test.d
+++ studyStm32/test.d
@@ -8,3 +8,5 @@
 ..\test.o: ..\HARDWARE\LED\led.h
 ..\test.o: ..\SYSTEM\usart\usart.h
 ..\test.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+..\test.o: ..\HARDWARE\EXTInterrupt\exti.h
+..\test.o: ..\HARDWARE\BEEP\beep.h
