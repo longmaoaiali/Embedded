a4c9342c06b2da01ff73da5fb57b5903df4c20ab
longmaoaiali <13213102491@163.com>
2021-08-24 22:37:44 +0800

    using timer come ture input capture

---
+++ studyStm32/HARDWARE/CAPTURE/capture.h
@@ -0,0 +1,10 @@
+#ifndef __CAPTURE_H
+#define __CAPTURE_H
+
+#include "sys.h"
+
+void TIM5_Cap_Init(u16 arr,u16 psc);
+
+
+#endif
+
---
+++ studyStm32/capture.d
@@ -0,0 +1,6 @@
+..\capture.o: ..\HARDWARE\CAPTURE\capture.c
+..\capture.o: ..\SYSTEM\sys\sys.h
+..\capture.o: ..\SYSTEM\sys\stm32f10x.h
+..\capture.o: ..\SYSTEM\sys\core_cm3.h
+..\capture.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\capture.o: ..\SYSTEM\sys\system_stm32f10x.h
